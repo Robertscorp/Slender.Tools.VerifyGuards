e3b42b4a6400ea6e305bed39c88fd30c8b3dc948
James Roberts <James@Robertscom.com>
2024-05-13 19:13:31 +1000

    Make Parameter internal
    #1

---
+++ src/Slender.Tools.VerifyGuards/Internals/Parameter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Slender.Tools.VerifyGuards.Internals
+{
+
+    internal class Parameter(bool IsNullable, Type ParameterType)
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public bool IsNullable { get; } = IsNullable;
+
+        public string Name { get; set; }
+
+        public Type ParameterType { get; } = ParameterType;
+
+        public object Value { get; set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        public object GetValue()
+            => this.Value ?? InstanceCache.GetInstance(this.ParameterType);
+
+        public bool IsInvalidNonNullableValueType()
+            => this.IsNullable && this.ParameterType.IsNonNullableValueType();
+
+        public bool IsInvalidNullableValueType()
+            => !this.IsNullable && this.ParameterType.IsNullableValueType();
+
+        #endregion Methods
+
+    }
+
+}
