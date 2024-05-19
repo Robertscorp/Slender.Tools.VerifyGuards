7e06555e3e9d452c63f525835a67b0e28cb3848b
James Roberts <James@Robertscom.com>
2024-05-19 19:08:13 +1000

    Update XML comments
    #1

--- src/Slender.Tools.VerifyGuards/Verify.cs
+++ src/Slender.Tools.VerifyGuards/Verify.cs
@@ -22,11 +22,20 @@ namespace Slender.Tools.VerifyGuards
         /// <param name="expression">
         /// An expression which represents an invocation, and the expected guards for the parameters of the invocation.<br/>
         /// <br/>
-        /// Supports constructors, and instance and static methods.<br/>
+        /// Supports constructors, instance methods, and static methods.<br/>
         /// <br/>
+        /// Constructors:
         /// <code>() => new Class(Is.Nullable&lt;T&gt;(), ...)</code>
-        /// <code>() => Class.StaticMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <br/>
+        /// Instance Methods:
         /// <code>(Class c) => c.InstanceMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <code>() => new Class(...).InstanceMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <code>() => classParameter.InstanceMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <code>() => _ClassVariable.InstanceMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <code>() => this.m_ClassField.InstanceMethod(Is.Nullable&lt;T&gt;(), ...)</code>
+        /// <br/>
+        /// Static Methods:
+        /// <code>() => Class.StaticMethod(Is.Nullable&lt;T&gt;(), ...)</code>
         /// </param>
         /// <exception cref="GuardException">Thrown when the actual guards within the invocation do not match the expected guards defined in the specified <paramref name="expression"/>.</exception>
         public static void Guards(LambdaExpression expression)
