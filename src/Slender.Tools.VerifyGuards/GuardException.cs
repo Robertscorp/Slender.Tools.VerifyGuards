5e626c3a625592b7b039537e9b461d5af6851667
James Roberts <James@Robertscom.com>
2024-05-19 19:57:08 +1000

    Make the constructor internal
    #1

--- src/Slender.Tools.VerifyGuards/GuardException.cs
+++ src/Slender.Tools.VerifyGuards/GuardException.cs
@@ -6,7 +6,15 @@ namespace Slender.Tools.VerifyGuards
     /// <summary>
     /// Represents errors that occur while verifying guard clauses.
     /// </summary>
-    /// <param name="Message">The message that describes the error.</param>
-    public class GuardException(string Message) : Exception(Message) { }
+    public class GuardException : Exception
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        internal GuardException(string message) : base(message) { }
+
+        #endregion Constructors
+
+    }
 
 }
